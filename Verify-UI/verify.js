b1796d78433ef853021ca6b1588c2129dd0bc40d
Alex Santamaria <santamariaalex146@gmail.com>
2021-09-26 23:34:23 -0500

    once a number is entered the focus goes to next input

--- Verify-UI/verify.js
+++ Verify-UI/verify.js
@@ -1,3 +1,15 @@
 const codes = document.querySelectorAll(".code");
 
+//puts cursor on first input when page loads
 codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (event) => {
+    if (event.key >= 0 && event.key <= 9) {
+      codes[idx].value = ""; //this fixes the backspace issue
+      setTimeout(() => codes[idx + 1].focus(), 10);
+    } else if (event.key === "Backspace") {
+      setTimeout(() => codes[idx - 1].focus(), 10);
+    }
+  });
+});
