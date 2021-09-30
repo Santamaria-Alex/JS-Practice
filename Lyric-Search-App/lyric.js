6269d2bfe0e66a8a66c4b225dd6ceb9358c3e150
Alex Santamaria <santamariaalex146@gmail.com>
2021-09-29 20:58:53 -0500

    completed JS. get lyrics button works, but cant get next button to work

--- Lyric-Search-App/lyric.js
+++ Lyric-Search-App/lyric.js
@@ -22,7 +22,7 @@ async function searchSongs(term) {
 
 //show song and artist in DOM
 function showData(data) {
-  let output = "";
+  //   let output = "";
 
   //   data.data.forEach((song) => {
   //     output += `
@@ -45,13 +45,39 @@ function showData(data) {
         ${data.data
           .map(
             (song) => `<li>
-        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <span class="span"><strong>${song.artist.name}</strong> - ${song.title}<br> <div id="album">${song.album.title}</div></span>
         <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
     </li>`
           )
           .join("")}
     </ul>
   `;
+  console.log(data);
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+//get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
 //event listener
@@ -66,3 +92,36 @@ form.addEventListener("submit", (e) => {
     searchSongs(searchTerm);
   }
 });
+
+//get lyrics button
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  //only works if clicked on button
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
+  }
+});
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  if (data.error) {
+    result.innerHTML = data.error;
+  } else {
+    //this part replaces returns or lines breaks w <br>
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+    result.innerHTML = `
+              <h2><strong>${artist}</strong> - ${songTitle}</h2>
+              <span>${lyrics}</span>
+          `;
+  }
+
+  more.innerHTML = "";
+}
