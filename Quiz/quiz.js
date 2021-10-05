c62499aed8ebde6d5f6c6570cfbd8aa4bda8309f
Alex Santamaria <santamariaalex146@gmail.com>
2021-10-05 17:54:31 -0500

    added event listener and checks for correct answers. need to intergrate api for questions

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -41,3 +41,63 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  //quiz data is the array w objects, at index currentQuiz which starts at 0
+  const currentQuizData = quizData[currentQuiz];
+
+  //getting the question object from quizData
+  questionEl.innerText = currentQuizData.question;
+
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEl.forEach((answerEl) => (answerEl.checked = false));
+}
+
+//gets the answer (abcd) that is clicked
+function getSelected() {
+  let answer;
+
+  answerEl.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++; //adds to your grade if correct
+    }
+
+    currentQuiz++; //goes to next question
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions corretcly</h2>
+
+        <button onClick="location.reload()">Reload Quiz</button>
+        `;
+    }
+  }
+
+  console.log(answer);
+});
