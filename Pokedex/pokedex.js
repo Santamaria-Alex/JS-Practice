369b681a11a5cf1ada3dc9459ed6f62b157b3a11
Alex Santamaria <santamariaalex146@gmail.com>
2021-10-14 20:22:12 -0500

    able to fetch data and add to DOM

--- Pokedex/pokedex.js
+++ Pokedex/pokedex.js
@@ -17,6 +17,11 @@ const colors = {
   normal: "#F5F5F5",
 };
 
+//makes the colors into an object with properties
+const main_types = Object.keys(colors);
+console.log(main_types);
+
+//gets pokemon data
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
     await getPokemon(i);
@@ -27,7 +32,44 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  createPokemonCard(data);
+  //   console.log(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  //to uppercase the first letter
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+  //makes the id 3 digits, if the id is single char, will add 0 in front
+  //ex: 001, 010, 100
+  const id = pokemon.id.toString().padStart(3, "0");
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  //   console.log(poke_types);
+
+  //get the type
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+
+  const pokemonInnerHTML = `
+    <div class="img-container">
+        <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+        alt=""
+        />
+  </div>
+  <div class="info">
+    <span class="number">#${id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>
+    `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  poke_container.appendChild(pokemonEl);
 };
 
 fetchPokemons();
