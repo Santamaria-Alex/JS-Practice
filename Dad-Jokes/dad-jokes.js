81dfe2b295d99e05758ddaa52aba3c045722d78a
Alex Santamaria <santamariaalex146@gmail.com>
2021-10-27 17:33:01 -0500

    same outcome, but used async await

--- Dad-Jokes/dad-jokes.js
+++ Dad-Jokes/dad-jokes.js
@@ -3,18 +3,32 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke() {
+//must label function as async when using await inside that function
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((response) => response.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const response = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await response.json();
+
+  jokeEl.innerHTML = data.joke;
+
+  // function generateJoke() {
+  //   const config = {
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   };
+
+  //   fetch("https://icanhazdadjoke.com", config)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       jokeEl.innerHTML = data.joke;
+  //     });
 }
 
 jokeBtn.addEventListener("click", generateJoke);
