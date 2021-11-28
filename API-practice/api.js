2589d01b17ba3b915ffb7b2203341bcf71008314
Alex Santamaria <santamariaalex146@gmail.com>
2021-11-28 16:06:17 -0600

    practicing with apis, able to fetch info from dictionary api

--- API-practice/api.js
+++ API-practice/api.js
@@ -1,3 +1,13 @@
 const container = document.getElementById("container");
 const btn = document.getElementById("btn");
 const search = document.getElementById("search");
+
+const test = document.getElementById("test");
+
+function dictionary() {
+  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+dictionary();
