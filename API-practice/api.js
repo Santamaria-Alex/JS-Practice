4e752f81ecdc256534d4a07e52746ea2e9d774e2
Alex Santamaria <santamariaalex146@gmail.com>
2021-11-28 20:26:35 -0600

    able to get input value and use that to search through dictionary api and post results onto DOM

--- API-practice/api.js
+++ API-practice/api.js
@@ -4,16 +4,25 @@ const search = document.getElementById("search");
 
 const test = document.getElementById("test");
 
-function dictionary() {
-  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+container.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchDictionary = search.value;
+
+  console.log(searchDictionary);
+
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchDictionary}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
       const info = data[0].meanings[0].definitions[0].definition;
+      const partOfSpeech = data[0].meanings[0].partOfSpeech;
+
+      console.log(partOfSpeech);
 
       console.log(info);
-    });
-}
 
-dictionary();
+      test.innerText = `${partOfSpeech}. ${info}`;
+    });
+});
