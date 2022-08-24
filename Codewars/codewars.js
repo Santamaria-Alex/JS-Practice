31c290d2a00ae488f7f9c4d62c9612fdf8c939a4
Alex Santamaria <santamariaalex146@gmail.com>
2022-08-24 02:46:48 -0500

    positiveSum

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -188,3 +188,15 @@ function digitize(n) {
   const x = Array.from(String(n), Number);
   return x.reverse();
 }
+//////////////////
+//8kyu
+//You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {
+  return arr
+    .filter((n) => {
+      return n > 0;
+    })
+    .reduce((a, b) => a + b, 0);
+}
