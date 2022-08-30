8be8e8f47d4e43b3cb2c6aac5bf7dee10e9f1895
Alex Santamaria <santamariaalex146@gmail.com>
2022-08-30 16:45:18 -0500

    even_or_odd

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -200,3 +200,9 @@ function positiveSum(arr) {
     })
     .reduce((a, b) => a + b, 0);
 }
+/////////////////
+//8kyu
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
