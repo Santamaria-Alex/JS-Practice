06e5dba4e5adee7d5451d3e64c7f6ed558062652
Alex Santamaria <santamariaalex146@gmail.com>
2022-09-02 18:05:57 -0500

    typeValidation

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -256,3 +256,8 @@ function isIsogram(str) {
 
   return x.length === str.toLowerCase().length ? true : false;
 }
+//8kyu
+//Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+function typeValidation(variable, type) {
+  return typeof variable === type;
+}
