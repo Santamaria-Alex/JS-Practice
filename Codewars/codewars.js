64f5055632de5bfa5740b56392ff4abb523de7a9
Alex Santamaria <santamariaalex146@gmail.com>
2022-09-02 14:08:51 -0500

    isIsogram

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -249,3 +249,10 @@ function XO(str) {
     str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
   );
 }
+//7kyu
+//An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+function isIsogram(str) {
+  const x = [...new Set(str.toLowerCase().split(""))];
+
+  return x.length === str.toLowerCase().length ? true : false;
+}
