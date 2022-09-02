4cb5c581d3b36b822b21f89ec9a5def9d8514b70
Alex Santamaria <santamariaalex146@gmail.com>
2022-09-02 10:37:07 -0500

    XO

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -242,3 +242,10 @@ function getMiddle(s) {
     s.length % 2 === 0 ? Math.ceil(s.length / 2 + 1) : Math.ceil(s.length / 2)
   );
 }
+//7kyu
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+function XO(str) {
+  return (
+    str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
+  );
+}
