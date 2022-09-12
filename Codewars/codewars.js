b9cc360646b950e58af94a7476933f139d0ccdb3
Alex Santamaria <santamariaalex146@gmail.com>
2022-09-12 12:59:12 -0500

    index

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -298,3 +298,8 @@ function squareOrSquareRoot(array) {
     return r % 1 == 0 ? r : x * x;
   });
 }
+//8kyu
+//You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+function index(array, n) {
+  return array[n] ? array[n] ** n : -1;
+}
