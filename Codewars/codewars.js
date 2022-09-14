78e95f089c54d3a3df4eeffdd4649a4026e55bb4
Alex Santamaria <santamariaalex146@gmail.com>
2022-09-13 19:26:03 -0500

    anagrams

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -308,3 +308,10 @@ function index(array, n) {
 function removeChar(str) {
   return str.slice(1, -1);
 }
+//5kyu
+//Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none.
+function anagrams(word, words) {
+  return words.filter(
+    (x) => x.split("").sort().join("") === word.split("").sort().join("")
+  );
+}
