b0f9a1319cbb853a3774b4218984dcd96a8defa2
Alex Santamaria <santamariaalex146@gmail.com>
2022-11-03 19:07:19 -0500

    boolToWord

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -341,3 +341,8 @@ function points(games) {
   }
   return total;
 }
+//8kyu
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
