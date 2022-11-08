c304bafbdf111c615a1ec9dc4f208714e1ab13b7
Alex Santamaria <santamariaalex146@gmail.com>
2022-11-07 23:58:56 -0600

    bmi

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -352,3 +352,15 @@ function isPalindrome(x) {
   const a = x.toLowerCase().split("").reverse().join("");
   return x.toLowerCase() === a ? true : false;
 }
+//8kyu
+//Write function bmi that calculates body mass index (bmi = weight / height2). if bmi <= 18.5 return "Underweight" ; if bmi <= 25.0 return "Normal" ; if bmi <= 30.0 return "Overweight" ;if bmi > 30 return "Obese"
+function bmi(weight, height) {
+  const b = weight / (height * height);
+  return b <= 18.5
+    ? "Underweight"
+    : b <= 25
+    ? "Normal"
+    : b <= 30
+    ? "Overweight"
+    : "Obese";
+}
